8fec16a46536fe9de25b55462200fc3ba0a7fa72
David Eads <davideads@gmail.com>
2015-02-09 10:51:03 -0500

    restore graphic, closes #66

--- www/js/app.js
+++ www/js/app.js
@@ -223,4 +223,10 @@ $(document).ready(function() {
 
     $(document).keydown(onDocumentKeyDown);
 
+    var pymParent = new pym.Parent(
+        'responsive-embed-syria-refugees-by-country',
+        'http://apps.npr.org/dailygraphics/graphics/syria-refugees-by-country/child.html',
+        {}
+    );
+
 });
