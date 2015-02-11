f7d059d6d29485a8c6d103af68b8bf395aee6be4
David Eads <davideads@gmail.com>
2015-02-11 10:46:16 -0500

    don't recursively crunch images, closes #89

--- crunch_images.sh
+++ crunch_images.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd www/assets/img
 for file in *.jpg; do
-    echo $file
-    convert $file -quality 80 -resize 1600 large-$file
-    convert $file -quality 80 -resize 1140 desktop-$file
-    convert $file -quality 80 -resize 768 mobile-$file
+    if [[ $file != desktop* ]] && [[ $file != mobile* ]] && [[ $file != large* ]];
+    then
+        echo "Making large-$file"
+        convert $file -quality 80 -resize 1600 large-$file
+        echo "Making desktop-$file"
+        convert $file -quality 80 -resize 1140 desktop-$file
+        echo "Making mobile-$file"
+        convert $file -quality 80 -resize 768 mobile-$file
+    fi
 done
 
 
