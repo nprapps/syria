36cd425155bb79256efbad93ddf8af9583710961
David Eads <davideads@gmail.com>
2015-02-13 23:10:51 -0500

    optimize images further

--- crunch_images.sh
+++ crunch_images.sh
@@ -3,12 +3,10 @@ cd www/assets/img
 for file in *.jpg; do
     if [[ $file != desktop* ]] && [[ $file != mobile* ]] && [[ $file != large* ]];
     then
-        echo "Making large-$file"
-        convert $file -quality 80 -resize 1600 large-$file
         echo "Making desktop-$file"
-        convert $file -quality 80 -resize 1140 desktop-$file
+        convert $file -strip -interlace Plane -gaussian-blur 0.03 -quality 80 -resize 1140 desktop-$file
         echo "Making mobile-$file"
-        convert $file -quality 80 -resize 768 mobile-$file
+        convert $file -strip -interlace Plane -gaussian-blur 0.03 -quality 80 -resize 768 mobile-$file
     fi
 done
 
